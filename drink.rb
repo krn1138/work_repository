c937c63a6928c46ae4f547ee35385e2bdaefa14f
sotakon <skon1221@gmail.com>
2021-02-23 21:41:07 +0900

    slot_money変更

--- drink.rb
+++ drink.rb
@@ -1,4 +1,4 @@
-#自販機商品（飲み物）クラス
+# 自販機商品（飲み物）クラス
 class Drink
   # 商品種別定義
   module Kind
@@ -8,61 +8,134 @@ class Drink
     WATER=3
   end
 
-  #以下は隠蔽
+  # 価格表（隠蔽）
   ITEMS_MASTER={
     Kind::COLA => {name: "cola", price: 120},
     Kind::REDBULL => {name: "redbull", price: 200},
     Kind::WATER => {name: "water", price: 100}
-  }.freeze
+  }
   private_constant :ITEMS_MASTER
 
   # インスタンス生成, 初期化
-  # 使用例1
-  # v = Drink.new(0, "potato", 50)
+  # 使用例1, 価格表に登録済みの飲み物は種別指定のみでOK
+  #   v = Drink.new(Kind::COLA)
   # 使用例2
-  # v = Drink.new(1)
+  #   v = Drink.new(4, "milk", 200)
+  # 注意, 指定された種別が数値で無い場合は例外をスローする
+  #   v = Drink.new("cola") --> DrinkInitializ
   def initialize(kind, name='', price=0)
-    @kind = kind
-    if[*1..3].include?(kind)
-      @name = ITEMS_MASTER[kind][:name]
-      @price = ITEMS_MASTER[kind][:price]
+    if(kind.class!=Integer)
+      raise DrinkInitializeError, "種別は正の整数値を指定してください. #{kind}.to_s"
     else
-      @name = name
-      @price = price
+      @kind = kind
+      if ITEMS_MASTER.keys.include?(kind)
+        @name = ITEMS_MASTER[kind][:name]
+        @price = ITEMS_MASTER[kind][:price]
+      else
+        @name = name
+        @price = price
+      end
     end
   end
 
-  #以下はインスタンスメソッド定義
-  # 使用例
-  # d = Drink.new(1); d.kind
+  # 以下はインスタンスメソッド定義
+
+  # 種別を返す
   def kind
     @kind
   end
 
+  # 名前を返す
   def name
     @name
   end
 
+  # 価格を返す
   def price
     @price
   end
 
-  #以下はクラスメソッド定義
+  # 以下はクラスメソッド定義
+  # 価格表から、指定した飲み物の価格を取得
   # 使用例
-  # price = Drink::price(1)
+  # price = Drink::price(Kind::COLA)
   def self.price (kind)
-    if ITEMS_MASTER.has_key?(kind)
+    if(kind.class!=Integer)
+      puts '種別は正の整数値を指定してください'   #return nil
+    elsif ITEMS_MASTER.has_key?(kind)
       ITEMS_MASTER[kind][:price]
     else
-      0
+      puts "specified kind is not exists."  #return nil
     end
   end
 
+  # 価格表から、指定した飲み物の名前を取得
+  # 使用例
+  # name =  Drink::name(Kind::COLA)
   def self.name (kind)
-    if ITEMS_MASTER.has_key?(kind)
+    if(kind.class!=Integer)
+      puts '種別は正の整数値を指定してください'   #return nil
+    elsif ITEMS_MASTER.has_key?(kind)
       ITEMS_MASTER[kind][:name]
     else
-      nil
+      puts "specified kind is not exists."  #return nil
     end
   end
+
+  # 価格表に、指定した飲み物の名前/価格を追加
+  # 使用例
+  # result =  Drink::insert(4, "milk", 200)
+  def self.insert(kind, name, price)
+    if(kind.class!=Integer)
+      puts '種別は正の整数値を指定してください'   #return nil
+    elsif ITEMS_MASTER.has_key?(kind)
+      puts "specified kind is already exists."  #return nil
+    else
+      ITEMS_MASTER[kind]={name: name, price: price}
+    end
+  end
+
+  # 価格表の、指定した飲み物の名前/価格を更新
+  # 使用例
+  # result =  Drink::update(Kind::COLA, "coke", 150)
+  def self.update(kind, name: name="Unspecified", price: price=-1)
+    if(kind.class!=Integer)
+      puts '種別は正の整数値を指定してください'   #return nil
+    elsif ITEMS_MASTER.has_key?(kind)
+      if ( (name != "Unspecified")  && !name.empty? )
+        ITEMS_MASTER[kind][:name]=name
+      end
+      if (price >= 0)
+        ITEMS_MASTER[kind][:price]=price
+      end
+      ITEMS_MASTER[kind]
+    else
+      puts "specified kind is not exists."  #return nil
+    end
+  end
+
+  # 価格表の、指定した飲み物の名前/価格を削除
+  # 使用例
+  # result = Drink::destroy(Kind::COLA)
+  def self.destroy(kind)
+    if(kind.class!=Integer)
+      puts '種別は正の整数値を指定してください'   #return nil
+    else
+      ITEMS_MASTER.delete(kind)  #return nil if kind is NOT exists
+    end
+  end
+
+  # 価格表の内容を文字列で出力
+  # 使用例
+  # result = Drink::master
+  def self.master
+    ITEMS_MASTER.each do |kind, item|
+      puts "種別:#{kind.to_s}, 名前:#{item[:name]}, 価格:#{item[:price]}\n"
+    end
+    # return ITEMS_MASTER
+  end
+end
+
+# 例外クラス定義
+class DrinkInitializeError < StandardError
 end
--- jihanki.rb
+++ jihanki.rb
@@ -1,3 +1,6 @@
+#require './jihanki.rb'
+#vm = VendingMachine.new
+
 require './drink.rb'
 require './inventory.rb'
 
@@ -22,15 +25,27 @@ class VendingMachine
     # 自動販売機に入っているお金を表示する
     @slot_money
   end
+
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   # 投入は複数回できる。
+  #ジュース値段以上の投入金額が投入されている条件下で購入操作を行うと、ジュースの在庫を減らし、売り上げ金額を増やす。
   def slot_money(money)
     # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
     # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
     return false unless MONEY.include?(money)
     # 自動販売機にお金を入れる
     @slot_money += money
+    #投入金額、在庫の点で購入可能なドリンクのリストを取得できる。
+    puts @stock.available_items(current_slot_money)
+    item = gets.to_i
+    if can_buy?(item)
+      @slot_money -= Drink::price(item)
+      return (@stock.pull_one(item)>0 ? true : false)
+    else
+      false
+    end
   end
+
   # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
   def return_money
     # 返すお金の金額を表示する
@@ -45,7 +60,7 @@ class VendingMachine
   end
 
   #投入金額、在庫の点で、コーラが購入できるかどうかを取得できる。
-  def can_buy(item)
+  def can_buy?(item)
     a = @stock.can_buy?(item , current_slot_money)
     if(a.class==Array)
       true
@@ -53,7 +68,7 @@ class VendingMachine
       false
     end
   end
-  
+
   #現在の売上金額を取得できる。
   def total_sales
     @stock.current_sales
@@ -69,14 +84,10 @@ class VendingMachine
     end
   end
 
+  #def return_moneyと同意味
   #払い戻し操作では現在の投入金額からジュース購入金額を引いた釣り銭を出力する。
   #ジュース値段以上の投入金額が投入されている条件下で購入操作を行うと、釣り銭（投入金額とジュース値段の差分）を出力する。
-  def refund(item)
-    purchase(item) ? @slot_money : false
-  end
-
- #投入金額、在庫の点で購入可能なドリンクのリストを取得できる。
-  def available_items
-    @stock.available_items(current_slot_money)
-  end
-end
+#   def refund(item)
+#     purchase(item) ? @slot_money : false
+#   end
+# end
